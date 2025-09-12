864e6f7c0c8db6d620beb5fa0b79ea3917a3cec5
sim-liberti <sim.liberti+github@gmail.com>
2025-09-12 17:30:46 +0200

    initial: scaffolding dei progetti

---
+++ WebApi/Program.cs
@@ -0,0 +1,38 @@
+namespace DietiEstate.WebApi;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder);
+        
+        var app = builder.Build();
+        ConfigureApplicationAsync(app);
+
+        app.Run();
+    }
+    
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        builder.Configuration
+            .SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+    }
+
+    private static void ConfigureApplicationAsync(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+        //app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
