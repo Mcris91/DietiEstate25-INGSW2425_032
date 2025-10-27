bf4d2782d32582dcbdb2ec13222af6427c8455bd
sim-liberti <sim.liberti+github@gmail.com>
2025-10-28 00:00:14 +0100

    init: aggiunta del progetto WebClient

---
+++ WebClient/Program.cs
@@ -0,0 +1,39 @@
+using System.Text.Json;
+using DietiEstate.WebClient.ApiService;
+using DietiEstate.WebClient.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+
+builder.Services.AddSingleton(new JsonSerializerOptions {
+    PropertyNameCaseInsensitive = true
+});
+builder.Services.AddHttpClient<ListingApiService>(client =>
+{
+    client.BaseAddress = new Uri($"{builder.Configuration["ApiUrl"]!}Listing/");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
